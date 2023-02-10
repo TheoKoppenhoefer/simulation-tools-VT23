0b943169ff2859a9a3434cb0bc12d24d93efea7a
Theo Koppenhoefer <koppenth@hotmail.de>
2023-02-10 10:40:46 +0100

    Implemented some experiments and plotting of the results.

--- Source/main_Project2.py
+++ Source/main_Project2.py
@@ -5,14 +5,14 @@ from squeezer2 import Seven_bar_mechanism_indx2
 from squeezer1 import Seven_bar_mechanism_indx1, Seven_bar_mechanism_expl
 import matplotlib.pyplot as mpl
 
-
 var_labels = [r'$\beta$', r'$\Theta$', r'$\gamma$', r'$\phi$', r'$\delta$', r'$\Omega$', r'$\epsilon$',
               r'$\dot{\beta}$', r'$\dot{\Theta}$', r'$\dot{\phi}$', r'$\dot{\delta}$', r'$\dot{\omega}$',
               r'$\dot{\Omega}$', r'$\dot{\epsilon}$', r'$\lambda_1$', r'$\lambda_2$', r'$\lambda_3$',
               r'$\lambda_4$', r'$\lambda_5$', r'$\lambda_6$', r'$\lambda_7$']
 
-def run_seven_bar_problem(with_plots=True, problem_index=3, atol_v=1E5, atol_lambda=1E5,
-                          algvar_v=False, algvar_lambda=False):
+
+def run_seven_bar_problem(with_plots=True, problem_index=3, atol_v=1E-6, atol_lambda=1E-6,
+                          algvar_v=False, algvar_lambda=False, suppress_alg=True):
     """
     """
     tfinal = 0.03  # Specify the final time
@@ -39,7 +39,7 @@ def run_seven_bar_problem(with_plots=True, problem_index=3, atol_v=1E5, atol_lam
         sim = IDA(mod)
 
         # Set the parameters
-        atol = 1E-6*np.ones((20,))
+        atol = 1E-6 * np.ones((20,))
         atol[7:14] = atol_v
         atol[14:20] = atol_lambda
         sim.atol = atol
@@ -47,11 +47,10 @@ def run_seven_bar_problem(with_plots=True, problem_index=3, atol_v=1E5, atol_lam
         algvar[7:14] = algvar_v
         algvar[14:20] = algvar_lambda
         sim.algvar = algvar
-        sim.suppress_alg = True
+        sim.suppress_alg = suppress_alg
 
         # Simulate
         t, y, yd = sim.simulate(tfinal)
-
 
     # Plot
     if with_plots:
@@ -60,18 +59,67 @@ def run_seven_bar_problem(with_plots=True, problem_index=3, atol_v=1E5, atol_lam
             mpl.plot(t, y[:, i])
         mpl.legend(var_labels[:7])
         mpl.figure()
-        for i in range(7,14):
+        for i in range(7, 14):
             mpl.plot(t, y[:, i])
         mpl.legend(var_labels[7:14])
         mpl.figure()
         if problem_index > 0:
-            for i in range(14,20):
+            for i in range(14, 20):
                 mpl.plot(t, y[:, i])
             mpl.legend(var_labels[14:])
         mpl.show()
     return mod, sim
 
 
+def plot_stats(xdata, ydata, plotnumber=100, savefig=False):
+    ylabels = ['nsteps', 'nfcns', 'njacs', 'nerrfails']
+    for i in range(4):
+        mpl.figure(plotnumber + i, clear=False)
+        # fig, ax = mpl.subplots()
+        mpl.bar(xdata, ydata[i])
+        mpl.ylabel(ylabels[i])
+        if savefig:
+            mpl.savefig(f'../Plots/Project2_main/Figure_{plotnumber + i}.pdf')
+    ylabels = ['nfcns / nsteps', 'njacs / nsteps', 'nerrfails / nsteps']
+    for i in range(3):
+        mpl.figure(plotnumber + 10 + i, clear=False)
+        mpl.bar(xdata, np.asarray(ydata[i + 1]) / np.asarray(ydata[0]))
+        mpl.ylabel(ylabels[i])
+        if savefig:
+            mpl.savefig(f'../Plots/Project2_main/Figure_{plotnumber + 10 + i}.pdf')
+
 
 if __name__ == '__main__':
-    run_seven_bar_problem(problem_index=0)
+    # run_seven_bar_problem(False, 1, 1E5, 1E5, False, False, True)
+
+    if True:
+        # list of experiments in the form [problem_index, atol_v, atol_lambda, algvar_v, algvar_lambda, suppress_alg]
+        experiments = [[1, 1E5, 1E5, False, False, True],
+                       [1, 1E-6, 1E5, False, True, True],
+                       [2, 1E-6, 1E5, False, False, True],
+                       [2, 1E-6, 1E5, False, True, True],
+                       [2, 1E-6, 1E5, False, True, True],
+                       [3, 1E5, 1E5, False, False, True],
+                       [3, 1E5, 1E-6, True, False, True]]
+
+        nsteps = []
+        nfcns = []
+        njacs = []
+        nerrfails = []
+        xdata = []
+        for exp in experiments:
+            try:
+                mod, sim = run_seven_bar_problem(False, *exp)
+
+                stats = sim.get_statistics()
+                xdata.append(
+                    f'problem_index={exp[0]} \n  atol_v={exp[1]:.0E} \n atol_lambda={exp[2]:.0E} \n algvar_v={exp[3]} \n algvar_lambda={exp[4]} \n suppress_alg={exp[5]}')
+                nsteps.append(stats.__getitem__('nsteps'))
+                nfcns.append(stats.__getitem__('nfcns'))
+                njacs.append(stats.__getitem__('njacs'))
+                nerrfails.append(stats.__getitem__('nerrfails'))
+            except:
+                print(f'There seems to be a problem in the experiment {exp}')
+
+        plot_stats(xdata, [nsteps, nfcns, njacs, nerrfails])
+        mpl.show()
