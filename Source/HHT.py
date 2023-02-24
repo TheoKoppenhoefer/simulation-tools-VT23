fb46ea8c37ffc03495b28deaed0a88b00747701e
Theo Koppenhoefer <koppenth@hotmail.de>
2023-02-24 17:41:53 +0100

    Some more bugfixes.

--- Source/HHT.py
+++ Source/HHT.py
@@ -129,8 +129,8 @@ class Newmark_implicit(Explicit_ODE):
 
         # Solve (8'') to get u_np1
         return spsolve(lhs_Matrix, f(t) \
-                        + M@ u_nm1/(beta*h**2+ud_nm1/(beta*h)+(1/(2*beta)-1)*udd_nm1) \
-                        + C@ u_nm1* gamma/(beta*h) -(1-gamma/beta)*ud_nm1-(1-gamma/(2*beta))*h*udd_nm1
+                        + M@( u_nm1/(beta*h**2+ud_nm1/(beta*h)+(1/(2*beta)-1)*udd_nm1) ) \
+                        + C@( u_nm1* gamma/(beta*h) -(1-gamma/beta)*ud_nm1-(1-gamma/(2*beta))*h*udd_nm1 )
                         + alpha*K@ u_nm1)
 
     def get_ud_n(self, u_n, u_nm1, ud_nm1, udd_nm1):
